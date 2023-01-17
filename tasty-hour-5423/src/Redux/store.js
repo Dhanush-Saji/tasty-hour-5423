8d0741db91e24debe3d8f5c40f034c2151996286
Dhanush Saji <dhanushsaji1@gmail.com>
2023-01-17 11:37:13 +0530

    dependencies updated

--- tasty-hour-5423/src/Redux/store.js
+++ tasty-hour-5423/src/Redux/store.js
@@ -1,5 +1,10 @@
-import React from 'react'
+import {legacy_createStore,combineReducers,applyMiddleware} from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from "redux-thunk";
 
-export const store = () => {
-    
-}
+const rootReducer = combineReducers({})
+
+
+// NOTE: use this store variable to create a store.
+export const store = legacy_createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk)));
+
