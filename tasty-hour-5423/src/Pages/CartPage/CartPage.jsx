1753ab0a855251f4f0dc88cd90b91a9d6bd8d831
HARSHAL-AP <apsundeharshal129@gmail.com>
2023-01-18 20:35:10 +0530

    fw18_1022 Day1 Task Complited

--- tasty-hour-5423/src/Pages/CartPage/CartPage.jsx
+++ tasty-hour-5423/src/Pages/CartPage/CartPage.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import "./Cart.css";
+import { Singlitem } from "./Singlitem";
+import { Loader } from "./Loader";
 
 const CartPage = () => {
   return (
-    <div>CartPage</div>
-  )
-}
+    <div className="Cartbody">
+      <div className="carthead">
+        <h1>My Cart(3 items)</h1>
+        <h1>Total Payable : ₹ 1373.8</h1>
+      </div>
+      <div className="listheading">
+        <div className="subhead">
+          <h1>Item Details</h1>
+          <h1>Quantity</h1>
+        </div>
+        <div className="subhead">
+          <h1>Delivery</h1>
+          <h1>Price</h1>
+        </div>
+      </div>
 
-export default CartPage
+      <div className="allcartprod">
+        <Singlitem />
+        <Singlitem />
+        <Singlitem />
+      </div>
+      <div className="nfjhfhthckdkkjd4">
+        <div className="addressbody87">
+          <h1 className="Addheadingcalss34">Dellivery To:</h1>
+          <p className="adresdescripton47">
+           No Adress Added yet...
+          </p>
+        </div>
+        <div className="payssbody87">
+          <h1 className="Addheadingcalss34">PRICE DETAILS</h1>
+          <div className="finalpric546"><h1>Price</h1><h1>₹2354</h1></div>
+          <div className="finalpric546_2"><h1>SubTotal</h1><h1>₹2354</h1></div>
+          <div className="palceorderbtn87">Place Order</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartPage;
---
+++ tasty-hour-5423/src/Pages/CartPage/Loader.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Loader.css";
+export const Loader = () => {
+  return (
+    <div class="lds-spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
