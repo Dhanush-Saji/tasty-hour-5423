8900ba0250c17e979053fe1f2242f3cff31e40d0
Rashmiranjan117 <107473816+Rashmiranjan117@users.noreply.github.com>
2023-01-20 21:46:33 +0530

    added login and signup pages with css and updated css on few pages

---
+++ tasty-hour-5423/src/Firebase/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+import "firebase/compat/auth";
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyAsDVY35_iBNOy4r7WPFwOSNtsaS-1a2Zg",
+    authDomain: "tasty-hour-5423.firebaseapp.com",
+    projectId: "tasty-hour-5423",
+    storageBucket: "tasty-hour-5423.appspot.com",
+    messagingSenderId: "19295056848",
+    appId: "1:19295056848:web:ba9f2d143da92913367fd5",
+    measurementId: "G-LFEQK0FVM0"
+});
