d5519a48166d619d2b0b58f05c8b6b5cede2a28b
HARSHAL-AP <apsundeharshal129@gmail.com>
2023-01-21 21:41:28 +0530

    day 5 completed

--- tasty-hour-5423/src/Redux/store.js
+++ tasty-hour-5423/src/Redux/store.js
@@ -1,12 +1,14 @@
 import { legacy_createStore, combineReducers, applyMiddleware } from "redux";
 import { reducer as ProductReducer } from "./Product_redux/reducer";
 import { reducer as AuthReducer } from "./AuthReducer/reducer";
+import {CartReducer} from "./cartReducer/reducer"
+
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
   ProductReducer,
-  AuthReducer,
+  AuthReducer,CartReducer
 });
 
 // NOTE: use this store variable to create a store.
