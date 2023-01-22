2a22ab156882c3acc4bcaa097b602d879c22e87d
Dhanush Saji <dhanushsaji1@gmail.com>
2023-01-22 22:44:21 +0530

    update

--- tasty-hour-5423/src/Pages/CheckoutPage/Fisitem.jsx
+++ tasty-hour-5423/src/Pages/CheckoutPage/Fisitem.jsx
@@ -4,7 +4,7 @@ import "./Fi.css"
 export const Fisitem = ({ el }) => {
   return (
     <div className="bodycontekdgjkjg">
-      <div className="imgcontaine"> <img src={el.images} alt="" /></div>
+      <div className="imgcontaine"> <img src={el.image} alt="" /></div>
       <div className="datacontainer"><h3>{el.name}</h3><p>Estimated delivery time 26 Jan 2023 09:59 PM</p></div>
     </div>
   );
