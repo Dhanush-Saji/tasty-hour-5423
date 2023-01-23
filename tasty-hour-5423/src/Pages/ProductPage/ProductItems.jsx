b1327f00bff9c6c3780de49b34eb41738b5dea2e
HARSHAL-AP <apsundeharshal129@gmail.com>
2023-01-23 11:30:16 +0530

    dsfgsd

--- tasty-hour-5423/src/Pages/ProductPage/ProductItems.jsx
+++ tasty-hour-5423/src/Pages/ProductPage/ProductItems.jsx
@@ -32,7 +32,7 @@ const ProductItems = (products) => {
     >
       <Box width="100%" height="227px">
         <Img
-          src={products.images}
+          src={products.image||products.images}
           height="100%"
           width="100%"
           onClick={() => {
